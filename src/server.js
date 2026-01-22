0a34d39ee1988c05ffb4ee537d0d633444d8d9f7
Kerlon Felipe Silva Carneiro <kerlonfsc1@gmail.com>
2026-01-22 19:27:36 -0300

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import statusRoutes from "./routes/status.routes.js";
 import cidadesRoutes from "./routes/cidades.routes.js";
 import roteirosRoutes from "./routes/roteiros.routes.js";
-import usuarioRoutes from "./routes/usuario.routes.js";
+import usuarioRoutes from "./routes/usuario.routes.js"; 
 
 dotenv.config();
 
@@ -17,10 +17,11 @@ app.use(express.json());
 app.use("/api/status", statusRoutes);
 app.use("/api/cidades", cidadesRoutes);
 app.use("/api/roteiros", roteirosRoutes);
-app.use("/api/usuario", usuarioRoutes);
+app.use("/api/usuario", usuarioRoutes); 
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta " + PORT);
 });
+
